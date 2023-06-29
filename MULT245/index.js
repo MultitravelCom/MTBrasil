c828fb0a252c4e9c3c86da37728e030f4d7d4b47
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-29 16:34:03 -0300

    br

---
+++ MULT245/index.js
@@ -0,0 +1,196 @@
+function wait(timeout) {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
+
+async function removeImageLinks() {
+    let images = document.querySelectorAll('.info-card__image-holder img');
+    images.forEach(function (image) {
+        image.addEventListener('click', function (event) {
+            event.preventDefault();
+        });
+    });
+}
+
+function removeDataTarget(resultsListPage) {
+    const items = resultsListPage.querySelectorAll('.results-list__item');
+
+    items.forEach(item => {
+        const card = item.querySelector('.info-card__image .info-card__image--action');
+
+        if (card) {
+            imageHolder.removeAttribute('data-target');
+        }
+    });
+
+    // Volver a verificar después de 100 ms
+    setTimeout(removeDataTarget, 100);
+}
+
+async function cargarEstilosYModales() {
+    const link = document.querySelector('link[href="https://multitravelcom.github.io/components/MULT245/style.css"]');
+    const scriptReact = document.querySelector('script[src="https://multitravelcom.github.io/components/MULT245/modalShare.js"]');
+
+    // Forzar la recarga del archivo CSS
+    if (link) {
+        link.href = '';
+        await wait(1000);
+        link.href = 'https://multitravelcom.github.io/components/MULT245/style.css';
+    }
+
+    // Forzar la recarga del script de los modales de React
+    if (scriptReact) {
+        scriptReact.src = '';
+        await wait(100);
+        scriptReact.src = 'https://multitravelcom.github.io/components/MULT245/modalShare.js';
+    }
+}
+
+async function aplicarClaseRecomendada(resultsListPage) {
+    const items = resultsListPage.querySelectorAll('.results-list__item');
+
+    items.forEach(item => {
+        const tieneDeals = item.querySelector('.deals') !== null;
+        const hotelResult = item.querySelector('.result.hotel-result');
+
+        if (tieneDeals && hotelResult) {
+            hotelResult.classList.add('alojamiento-recomendado');
+
+            const bestPriceElements = item.querySelectorAll('.info-card__price');
+            bestPriceElements.forEach(element => {
+                element.classList.add('info-card__price__deals');
+            });
+
+            const toggleButtons = item.querySelectorAll('.info-card__options-toggle');
+            toggleButtons.forEach(button => {
+                button.classList.add('right-14px');
+            });
+        }
+    });
+}
+
+async function agreeStarIcon(resultsListPage) {
+    const items = resultsListPage.querySelectorAll('.results-list__item');
+
+    items.forEach(item => {
+        const infoCardCategory = item.querySelector('.info-card__category');
+
+        const newSpaninfoCardCategory = document.createElement('span');
+        newSpaninfoCardCategory.className = 'glyphicon glyphicon-star iconStar';
+
+        infoCardCategory.insertBefore(newSpaninfoCardCategory, infoCardCategory.firstChild);
+    });
+}
+
+async function changeCopyMap(resultsListPage) {
+    let items = resultsListPage.querySelectorAll('.results-list__item');
+
+    items.forEach(item => {
+        let mapLink = item.querySelector('.map-link');
+        let locationIcon = mapLink.querySelector('.info-card__location-icon');
+
+        mapLink.lastChild.textContent = 'Ver Mapa';
+        mapLink.style.display = 'block';
+    });
+}
+
+async function applyDisplayNoneToAllButLastButton(resultsListPage) {
+    const actionsContainer = resultsListPage.querySelector('.info-card__actions');
+
+    if (!actionsContainer) {
+        await wait(2000);
+        await applyDisplayNoneToAllButLastButton(resultsListPage);
+        return;
+    }
+    const buttonsAndLinks = actionsContainer.querySelectorAll('button, a');
+
+    for (let i = 0; i < buttonsAndLinks.length; i++) {
+        if (i !== buttonsAndLinks.length - 1) {
+            buttonsAndLinks[i].style.display = 'none';
+        }
+    }
+
+    const lastButtonContainer = buttonsAndLinks[buttonsAndLinks.length - 1].parentNode;
+    lastButtonContainer.style.display = 'inline-block';
+
+    const buttonsVerDetalle = document.querySelectorAll('.info-card__options-toggle');
+
+    for (let i = 0; i < buttonsVerDetalle.length; i++) {
+        buttonsVerDetalle[i].textContent = 'Comprar';
+    }
+}
+
+async function changeCopyButton(resultsListPage) {
+    const itemsButtonComprar = resultsListPage.querySelectorAll('.results-list__item');
+
+    itemsButtonComprar.forEach(item => {
+        const buttonElement = item.querySelector('.info-card__options-toggle');
+        buttonElement.textContent = 'Comprar';
+        buttonElement.style.display = 'block';
+
+        // buttonElement.addEventListener('click', (event) => {
+        //     event.preventDefault();
+        //     const link = item.querySelector('a.abs');
+        //     if (link) {
+        //         window.open(link.href, '_blank');
+        //     }
+        // });
+    });
+
+    const checkResultsListPage = () => {
+        const resultsPage = document.querySelector('.results-list__page');
+
+        if (resultsPage) {
+            const resultsListPage = resultsPage.querySelectorAll('.results-list__item');
+
+            resultsListPage.forEach(item => {
+                const selectors = item.querySelectorAll('.info-card__image-holder');
+
+                selectors.forEach(selector => {
+                    selector.classList.remove('js-open-gallery');
+                });
+            });
+        } else {
+            setTimeout(checkResultsListPage, 2000);
+        }
+    };
+
+    checkResultsListPage();
+};
+
+
+function aplicarModificaciones(resultsListPage) {
+    removeDataTarget(resultsListPage);
+    removeImageLinks(resultsListPage);
+    aplicarClaseRecomendada(resultsListPage);
+    agreeStarIcon(resultsListPage);
+    changeCopyMap(resultsListPage);
+    applyDisplayNoneToAllButLastButton(resultsListPage);
+    changeCopyButton(resultsListPage);
+}
+
+function observarCambiosCheckAndRender() {
+    const observerConfig = {
+        rootNode: document.documentElement,
+        callback: () => {
+            requestAnimationFrame(() => {
+                const resultsListPages = document.querySelectorAll('.results-list__page');
+                resultsListPages.forEach(resultsListPage => {
+                    aplicarModificaciones(resultsListPage);
+                });
+            });
+        },
+        queries: [{ element: '.results-list__page' }],
+    };
+
+    const observer = new MutationSummary(observerConfig);
+
+    const resultsListPages = document.querySelectorAll('.results-list__page');
+    resultsListPages.forEach(resultsListPage => {
+        aplicarModificaciones(resultsListPage);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
+    observarCambiosCheckAndRender();
+    cargarEstilosYModales();
+});
---
+++ MULT245/modalShare.js
@@ -0,0 +1,120 @@
+function ButtonModalShare(props) {
+    const handleClick = (event) => {
+        event.preventDefault();
+        props.onClick();
+    };
+
+    return (
+        <button id={props.id} className={props.style} onClick={handleClick}>
+            {props.children}
+        </button>
+    );
+}
+
+const ModalShare = ({ open }) => {
+    React.useEffect(() => {
+        const modalElement = document.getElementById("modal-social");
+        if (modalElement) {
+            modalElement.style.display = open ? "block" : "none";
+        }
+    }, [open]);
+
+    return null;
+};
+
+const CompartirAlojamiento = () => {
+    const [openModal, setOpenModal] = React.useState(false);
+    return (
+        <>
+            <div className="cont">
+                <ButtonModalShare
+                    onClick={() => setOpenModal(true)}
+                    style="main__container__widget__share"
+                >
+                    <div className="main__container__share js-social-share">
+                        <span className="glyphicon glyphicon-share share__icon"></span>
+                        <span className="share__text">Compartí este alojamiento ahora</span>
+                    </div>
+                </ButtonModalShare>
+                <ModalShare open={openModal} onClose={() => setOpenModal(false)} />
+            </div>
+        </>
+    )
+}
+const BannerMensageCard = ({ text_p }) => {
+    return (
+        <div className="main__container__bannerMensageCard">
+            <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
+            <p>{text_p}</p>
+        </div>
+    )
+}
+
+const BannerMensageCardApp = () => {
+    const [hasBestPriceTaxIncluded, setHasBestPriceTaxIncluded] = React.useState(true);
+
+    React.useEffect(() => {
+        const checkDivPresence = () => {
+            const div = document.querySelector('.bestprice__taxincluded');
+            setHasBestPriceTaxIncluded(!div);
+        };
+
+        checkDivPresence();
+        const interval = setInterval(checkDivPresence, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    return (
+        <>
+            {hasBestPriceTaxIncluded ? (
+                <BannerMensageCard text_p={"Pagá hasta en 12 cuotas fijas"} />
+            ) : (
+                <BannerMensageCard text_p={"Comprá ahora y congela el precio en pesos"} />
+            )}
+        </>
+    );
+};
+
+const checkAndRender = async () => {
+    let infoCardContents = document.querySelectorAll('.info-card__content');
+
+    while (infoCardContents.length === 0) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        infoCardContents = document.querySelectorAll('.info-card__content');
+    }
+
+    infoCardContents.forEach(infoCardContent => {
+        const nuevoDiv = document.createElement('div');
+        const nuevoDivBannerMensage = document.createElement('div');
+
+        infoCardContent.appendChild(nuevoDiv);
+        infoCardContent.appendChild(nuevoDivBannerMensage);
+
+        nuevoDivBannerMensage.classList.add('main__container__bannerMensageCard__App');
+
+
+        ReactDOM.render(<CompartirAlojamiento />, nuevoDiv);
+        ReactDOM.render(<BannerMensageCardApp />, nuevoDivBannerMensage);
+    });
+};
+checkAndRender();
+
+function observarCambiosCheckAndRender() {
+    const observerConfig = {
+        rootNode: document.documentElement,
+        callback: () => {
+
+            requestAnimationFrame(checkAndRender);
+        },
+        queries: [{ element: '.results-list__page' }],
+    };
+
+    const observer = new MutationSummary(observerConfig);
+
+    checkAndRender();
+}
+
+observarCambiosCheckAndRender();
