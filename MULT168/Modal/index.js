15b830d7aa0a4ba1b88f46c17483fa2b609578bd
Multitravel.com <ignacio.peralta@multitravel.com>
2023-06-30 14:26:42 -0300

    img

--- MULT168/Modal/index.js
+++ MULT168/Modal/index.js
@@ -19,14 +19,17 @@ function addHideDuplicateButtonLogic() {
         buttonsBtnUser[1].classList.add('hidden');
     }
 }
+//***********************************Ocultar nacionalidad*************************
 function ocultarNacionalidad() {
     const element = document.querySelector('label[for="hotel-searcher-nationality"]');
     const parent = element.closest('.col-xs-12.col-sm-12.col-md-3');
     parent.style.display = 'none';
 
-    const elementFixedProduct = document.querySelector('.searcher__fixed-product');
-    const parentFixedProduct = elementFixedProduct.closest('.col-xs-12.col-sm-12.col-md-3');
-    parentFixedProduct.style.width = '45%';
+    if (window.innerWidth >= 768) {
+        const elementFixedProduct = document.querySelector('.searcher__fixed-product');
+        const parentFixedProduct = elementFixedProduct.closest('.col-xs-12.col-sm-12.col-md-3');
+        parentFixedProduct.style.width = '45%';
+    }
 }
 ocultarNacionalidad()
 addHideDuplicateButtonLogic();
