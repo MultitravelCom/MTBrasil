0b5a1b399483dd5d232ca49b1c3faba0442c5e77
Multitravel.com <ignacio.peralta@multitravel.com>
2023-07-05 10:57:33 -0300

    modal

--- MULT168/Modal/index.js
+++ MULT168/Modal/index.js
@@ -20,19 +20,19 @@ function addHideDuplicateButtonLogic() {
     }
 }
 //***********************************Ocultar nacionalidad*************************
-function ocultarNacionalidad() {
-    const element = document.querySelector('label[for="hotel-searcher-nationality"]');
-    const parent = element.closest('.col-xs-12.col-sm-12.col-md-3');
-    parent.style.display = 'none';
+// function ocultarNacionalidad() {
+//     const element = document.querySelector('label[for="hotel-searcher-nationality"]');
+//     const parent = element.closest('.col-xs-12.col-sm-12.col-md-3');
+//     parent.style.display = 'none';
 
-    if (window.innerWidth >= 768) {
-        const elementFixedProduct = document.querySelector('.searcher__fixed-product');
-        const parentFixedProduct = elementFixedProduct.closest('.col-xs-12.col-sm-12.col-md-3');
-        parentFixedProduct.style.width = '45%';
-    }
-}
-ocultarNacionalidad()
-addHideDuplicateButtonLogic();
+//     if (window.innerWidth >= 768) {
+//         const elementFixedProduct = document.querySelector('.searcher__fixed-product');
+//         const parentFixedProduct = elementFixedProduct.closest('.col-xs-12.col-sm-12.col-md-3');
+//         parentFixedProduct.style.width = '45%';
+//     }
+// }
+// ocultarNacionalidad()
+// addHideDuplicateButtonLogic();
 
 function ButtonARSEs() {
     return (
