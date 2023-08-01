dcf4353c5c53c03b07947d6bf6abb3e46f108395
SebastianFournier <sebastian.fournier@multitravel.com>
2023-08-01 15:57:10 -0300

    Change observer

--- POC-CLICKBOARD/index.js
+++ POC-CLICKBOARD/index.js
@@ -1,14 +1,13 @@
-
-
+   
 const CompartirAlojamiento = () => {
     const [openModal, setOpenModal] = React.useState(false);
     return (
         <>
-    <div className="div__clickboard">
-        <span className="glyphicon glyphicon-new-window new-window__icon"></span>
-        <span className="new-window__text">Copia el codigo de tu cupon!</span>
-    </div>
-    </>
+            <div className="div__clickboard">
+                <span className="glyphicon glyphicon-new-window new-window__icon"></span>
+                <span className="new-window__text">Copia el codigo de tu cupon!</span>
+            </div>
+        </>
     );
 };
 
@@ -20,6 +19,30 @@ const agregarClickboardButton = async () => {
     let infoCardContents = document.querySelectorAll('.info-card__content');
     let infoCardImgContents = document.querySelectorAll('.info-card__image');
     */
+    
+    
+};
+
+
+
+/*
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("window.location.href ------->>>>> ", window.location.href)
+    if (window.location.href.includes('https://onefiveb2b.juniperbetemp.com/flights/')) {
+        agregarClickboardButton();
+    }else if (window.location.href.includes('/flights/booking')){
+      changeStyleBooking();
+    }
+  });
+
+  */
+
+  const insertNewDivSearchResult = async () => {
+    const observer = new MutationSummary({
+        callback: handleDOMChanges,
+        queries: [{ element: '.bannerTop__center' }]
+    });
+
     let bannerContainer = document.querySelector('.bannerTop__center');
    
     const nuevoDiv = document.createElement('div');
@@ -32,15 +55,6 @@ const agregarClickboardButton = async () => {
     nuevoDiv.style.backgroundColor= 'blue';
 
     ReactDOM.render(<CompartirAlojamiento />, nuevoDiv);
-    
 };
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("window.location.href ------->>>>> ", window.location.href)
-    if (window.location.href.includes('https://onefiveb2b.juniperbetemp.com/flights/')) {
-        agregarClickboardButton();
-    }else if (window.location.href.includes('/flights/booking')){
-      changeStyleBooking();
-    }
-  });
+insertNewDivSearchResult();
