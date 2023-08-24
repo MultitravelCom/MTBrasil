1531487c10399960741ce832e9034f4f8edd1b92
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-08-24 12:53:10 -0300

    ocultar cuil/cuit

--- OcultarCamposBookin2/index.js
+++ OcultarCamposBookin2/index.js
@@ -1,18 +1,40 @@
 // Función para buscar y ocultar el elemento
 function hideElement() {
-    // Encuentra todos los elementos con la clase confirm-booking__field col-sm-6
-    const elementsToHide = document.querySelectorAll('.confirm-booking__field.col-sm-6');
+    // Encuentra el contenedor que contiene los campos
+    const fieldsContainer = document.querySelector('.confirm-booking__fields');
+
+    // Busca todos los elementos con la clase confirm-booking__field col-sm-6 dentro del contenedor
+    const elementsToHide = fieldsContainer.querySelectorAll('.confirm-booking__field.col-sm-6');
 
     // Recorre los elementos y verifica si contienen el texto "CUIT/CUIL"
     elementsToHide.forEach(element => {
         const fieldLabel = element.querySelector('.confirm-booking__field-label');
         if (fieldLabel && fieldLabel.textContent.trim() === 'CUIT/CUIL') {
             element.style.display = 'none';
-            // Si encuentras y ocultas el elemento, puedes detener el bucle aquí
-            return;
         }
     });
 }
 
+function modifyFieldsText() {
+    console.log('Ejecutando la función modifyFieldsText...');
+    // Encuentra el contenedor que contiene los campos
+    const fieldsContainer = document.querySelector('.confirm-booking__fields');
+  
+    // Busca todos los elementos con la clase confirm-booking__field col-sm-5 y col-sm-6 dentro del contenedor
+    const elementsToModify = fieldsContainer.querySelectorAll('.confirm-booking__field.col-sm-5, .confirm-booking__field.col-sm-6');
+  
+    // Recorre los elementos y realiza las modificaciones necesarias
+    elementsToModify.forEach(element => {
+      const fieldLabel = element.querySelector('.confirm-booking__field-label');
+      if (fieldLabel) {
+        const labelContent = fieldLabel.textContent.trim();
+        if (labelContent === 'Documento de identidade / Passaporte') {
+          fieldLabel.textContent = 'CPF/CNPJ';
+        }
+      }
+    });
+  }
+
 // Llama a la función cuando sea necesario
 hideElement();
+modifyFieldsText();
