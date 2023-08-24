4638a3fe66f28f8c4c183d1fb8dac8ac430812e1
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-08-24 12:36:20 -0300

    ocultar cuil/cuit

--- OcultarCamposBookin2/index.js
+++ OcultarCamposBookin2/index.js
@@ -2,15 +2,18 @@ let elementToHide = null;
 
 // Función para buscar y ocultar el elemento
 function hideElement() {
-    if (elementToHide === null) {
-        // Encuentra el elemento por el selector
-        elementToHide = document.querySelector('.confirm-booking__field.col-sm-6');
-    }
+    // Encuentra todos los elementos con la clase confirm-booking__field col-sm-6
+    const elementsToHide = document.querySelectorAll('.confirm-booking__field.col-sm-6');
 
-    // Si se encontró el elemento, ocúltalo
-    if (elementToHide) {
-        elementToHide.style.display = 'none';
-    }
+    // Recorre los elementos y verifica si contienen el texto "CUIT/CUIL"
+    elementsToHide.forEach(element => {
+        const fieldLabel = element.querySelector('.confirm-booking__field-label');
+        if (fieldLabel && fieldLabel.textContent.trim() === 'CUIT/CUIL') {
+            element.style.display = 'none';
+            // Si encuentras y ocultas el elemento, puedes detener el bucle aquí
+            return;
+        }
+    });
 }
 
 // Llama a la función cuando sea necesario
