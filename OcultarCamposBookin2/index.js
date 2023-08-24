746550139048df34ea8e5706f280be85e84f43b7
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-08-24 12:57:12 -0300

    ocultar cuil/cuit

--- OcultarCamposBookin2/index.js
+++ OcultarCamposBookin2/index.js
@@ -19,22 +19,24 @@ function modifyFieldsText() {
     console.log('Ejecutando la función modifyFieldsText...');
     // Encuentra el contenedor que contiene los campos
     const fieldsContainer = document.querySelector('.confirm-booking__fields');
-  
+
     // Busca todos los elementos con la clase confirm-booking__field col-sm-5 y col-sm-6 dentro del contenedor
     const elementsToModify = fieldsContainer.querySelectorAll('.confirm-booking__field.col-sm-5, .confirm-booking__field.col-sm-6');
-  
+
     // Recorre los elementos y realiza las modificaciones necesarias
     elementsToModify.forEach(element => {
-      const fieldLabel = element.querySelector('.confirm-booking__field-label');
-      if (fieldLabel) {
-        const labelContent = fieldLabel.textContent.trim();
-        if (labelContent === 'Documento de identidade / Passaporte') {
-          fieldLabel.textContent = 'CPF/CNPJ';
+        const fieldLabel = element.querySelector('.confirm-booking__field-label');
+        if (fieldLabel) {
+            const labelContent = fieldLabel.textContent.trim();
+            if (labelContent === 'Documento de identidade / Passaporte') {
+                fieldLabel.textContent = 'CPF/CNPJ';
+            }
         }
-      }
     });
-  }
+}
 
 // Llama a la función cuando sea necesario
-hideElement();
-modifyFieldsText();
+document.addEventListener("DOMContentLoaded", function () {
+    hideElement();
+    modifyFieldsText();
+});
