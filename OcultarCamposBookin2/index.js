6486ccba3f095ed06d068210d78f69f3aa43fc0b
Multitravel.com <ignacio.peralta@multitravel.com>
2023-08-28 11:43:56 -0300

    cambio copy

--- OcultarCamposBookin2/index.js
+++ OcultarCamposBookin2/index.js
@@ -37,21 +37,16 @@ function modifyFieldsTextFlights() {
     });
 }
 function modifyFieldsTextBooking() {
-    // if (window.location.href.includes('/confirmBooking')) {
-    //     return; // No ejecutar la función
-    // }
-    // Encuentra el contenedor que contiene los campos
-    const fieldsContainer = document.querySelector('.confirm-booking__fields');
-
-    // Busca todos los elementos con la clase confirm-booking__field col-sm-5 y col-sm-6 dentro del contenedor
-    const elementsToModify = fieldsContainer.querySelectorAll('.confirm-booking__field.col-sm-5');
+    console.log('Ejecutando la función modifyFieldsTextBooking()...');
+    // Encuentra todos los elementos con la clase booking-paxes__pax-field
+    const elementsToModify = document.querySelectorAll('.booking-paxes__pax-field');
 
     // Recorre los elementos y realiza las modificaciones necesarias
     elementsToModify.forEach(element => {
-        const fieldLabel = element.querySelector('.confirm-booking__field-label');
+        const fieldLabel = element.querySelector('.booking-paxes__pax-field-label');
         if (fieldLabel) {
-            const labelContent = fieldLabel.textContent.trim();
-            if (labelContent === 'Documento de identidade / Passaporte') {
+            const labelText = fieldLabel.textContent.trim();
+            if (labelText === 'Documento de identidade / Passaporte') {
                 fieldLabel.textContent = 'CPF/CNPJ*';
             }
         }else {
