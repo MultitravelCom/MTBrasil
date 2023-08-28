72f1f1dd2f47307e16664261acd8afb2809eaeba
Multitravel.com <ignacio.peralta@multitravel.com>
2023-08-28 11:50:38 -0300

    cambio copy

--- OcultarCamposBookin2/index.js
+++ OcultarCamposBookin2/index.js
@@ -36,24 +36,33 @@ function modifyFieldsTextFlights() {
         }
     });
 }
+
 function modifyFieldsTextBooking() {
+    if (window.location.href.includes('/flights')) {
+        return; // No ejecutar la función
+    }
     console.log('Ejecutando la función modifyFieldsTextBooking()...');
-    // Encuentra todos los elementos con la clase booking-paxes__pax-field
-    const elementsToModify = document.querySelectorAll('.booking-paxes__pax-field');
+
+    // Encuentra el contenedor que contiene los campos
+    const fieldsContainer = document.querySelector('.confirm-booking__fields');
+
+    // Busca todos los elementos con la clase confirm-booking__field col-sm-5
+    const elementsToModify = fieldsContainer.querySelectorAll('.confirm-booking__field.col-sm-5');
 
     // Recorre los elementos y realiza las modificaciones necesarias
     elementsToModify.forEach(element => {
-        const fieldLabel = element.querySelector('.confirm-booking__fields');
+        const fieldLabel = element.querySelector('.confirm-booking__field-label');
         if (fieldLabel) {
-            const labelText = fieldLabel.textContent.trim();
-            if (labelText === 'Documento de identidade / Passaporte') {
-                fieldLabel.textContent = 'CPF/CNPJ*';
+            const labelContent = fieldLabel.textContent.trim();
+            if (labelContent === 'Documento de identidade / Passaporte') {
+                fieldLabel.textContent = 'CPF/CNPJ';
             }
         }else {
             setTimeout(modifyFieldsTextBooking, 100);
         }
     });
 }
+
 function changeDocumentText() {
     console.log('Ejecutando la función changeDocumentText...');
     // Encuentra todos los elementos con la clase booking-paxes__pax-field
