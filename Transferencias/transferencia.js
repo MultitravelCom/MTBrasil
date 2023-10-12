d983f3f2bf89bc67418479f90c9ada165e26874a
Multitravel.com <ignacio.peralta@multitravel.com>
2023-10-12 14:50:24 -0300

    fixes

--- Transferencias/transferencia.js
+++ Transferencias/transferencia.js
@@ -259,19 +259,19 @@ const TransferenciaComponent = () => {
                 <picture>
                     <source
                         media="(min-width: 1024px)"
-                        srcSet="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenDesktop.webp"
+                        srcSet="https://multitravelcom.github.io/MTBrasil-Img/Booking-2/ComoPagar/ImagenDesktop.webp"
                     />
                     <source
                         media="(min-width: 768px) and (max-width: 1023px)"
-                        srcSet="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenTablet.webp"
+                        srcSet="https://multitravelcom.github.io/MTBrasil-Img/Booking-2/ComoPagar/ImagenTablet.webp"
                     />
                     <source
                         media="(max-width: 767px)"
-                        srcSet="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenMobile.webp"
+                        srcSet="https://multitravelcom.github.io/MTBrasil-Img/Booking-2/ComoPagar/ImagenMobile.webp"
                     />
                     <img
                         alt=""
-                        src="https://multitravelcom.github.io/MT/Secciones/Booking-2/ComoPagar/ImagenDesktop.webp"
+                        src="https://multitravelcom.github.io/MTBrasil-Img/Booking-2/ComoPagar/ImagenDesktop.webp"
                     />
                 </picture>
             </div>
