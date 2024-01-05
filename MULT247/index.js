27e170b4782844d6d92a6bbd70ccfb98016fe489
Multitravel.com <ignacio.peralta@multitravel.com>
2024-01-05 15:29:37 -0300

    mult-247

---
+++ MULT247/index.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const selectors = [
+        ".details-card__amenities .details-card__section",
+        ".details-card__description.details-card__section"
+        // Agrega aquí los selectores adicionales que desees
+    ];
+
+    selectors.forEach(function (selector) {
+        const sections = document.querySelectorAll(selector);
+
+        sections.forEach(function (section) {
+            const content = section.querySelector(".details-card__section-content");
+
+            // Establecer un valor predeterminado para content.style.display
+            content.style.display = "none";
+
+            section.addEventListener("click", function () {
+                if (content.style.display === "none") {
+                    content.style.display = "block";
+                    icon.style.transform = "rotate(180deg)";
+                } else {
+                    content.style.display = "none";
+                    icon.style.transform = "rotate(0deg)";
+                }
+            });
+
+            const icon = document.createElement('div');
+            icon.className = "glyphicon glyphicon-chevron-down chevron-down-hotels";
+            section.appendChild(icon);
+        });
+    });
+});
