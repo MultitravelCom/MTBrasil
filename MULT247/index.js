cfc21554bb751c6392ed8bed8b088700d37f5c90
Multitravel.com <ignacio.peralta@multitravel.com>
2024-01-05 16:07:14 -0300

    mult-247

--- MULT247/index.js
+++ MULT247/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const selectors = [
         ".details-card__amenities .details-card__section",
         ".details-card__description.details-card__section",
-        "details-card__section"
+        ".details-card__section"
         // Agrega aquí los selectores adicionales que desees
     ];
 
