c9fd4cdcea9422d2114bab04c7b082a15e0ac483
Multitravel.com <ignacio.peralta@multitravel.com>
2024-01-12 12:50:50 -0300

    dsfdfs

--- MULT247/index.js
+++ MULT247/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const selectors = [
-        "#main-content > div > article > section.details-content > div.details-card__section.details-card__description",
+        ".details-card__section.details-card__description",
         ".details-card__amenities.details-card__section",
         ".details-card__description.details-card__section",
         ".details-card__section.details-card__section"
@@ -14,20 +14,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // Establecer un valor predeterminado para content.style.display
             content.style.display = "none";
-            
-            const icon = document.createElement('div');
-            icon.className = "glyphicon glyphicon-chevron-down chevron-down-hotels";
-            section.appendChild(icon);
 
-            section.addEventListener("click", function () {
-                if (content.style.display === "none") {
-                    content.style.display = "block";
-                    icon.style.transform = "rotate(180deg)";
-                } else {
-                    content.style.display = "none";
-                    icon.style.transform = "rotate(0deg)";
-                }
-            });
+            if (section.classList.contains('details-card__description')) {
+                const icon = document.createElement('div');
+                icon.className = "glyphicon glyphicon-chevron-down chevron-down-hotels";
+                section.appendChild(icon);
+
+                section.addEventListener("click", function () {
+                    if (content.style.display === "none") {
+                        content.style.display = "block";
+                        icon.style.transform = "rotate(180deg)";
+                    } else {
+                        content.style.display = "none";
+                        icon.style.transform = "rotate(0deg)";
+                    }
+                });
+            }
 
         });
     });
