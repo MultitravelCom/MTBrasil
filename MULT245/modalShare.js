5de419f67217af6dcd12f9d919eae19fb6df92d9
Multitravel.com <ignacio.peralta@multitravel.com>
2024-01-12 11:32:41 -0300

    dsfdfs

--- MULT245/modalShare.js
+++ MULT245/modalShare.js
@@ -268,7 +268,7 @@ const zonasTravelSale = [
 //   *************************************************************
 // ********************** Timer *****************************
 function shouldShowEvent() {
-    const startDate = new Date("2023-08-27T22:00:00");
+    const startDate = new Date("2023-08-27T22:00:00");|
     const endDate = new Date("2023-11-15T23:30:00");
     const now = new Date();
 
@@ -377,15 +377,15 @@ const BannerMensageCardApp = () => {
     return (
         <>
             {showFreezePriceMessageA && (
-                <BannerMensageCard text_p={"Aprovechá el descuento con el código: 50MILOFF"} />
+                <BannerMensageCard text_p={ "Pague em até 12 x sem juros"} />
             )}
 
             {showFreezePriceMessageB && (
-                <BannerMensageCard text_p={"Aprovechá el descuento con el código: 50MILOFF"} />
+                <BannerMensageCard text_p={ "Pague em até 12 x sem juros"} />
             )}
 
             {showFreezePriceMessageC && (
-                <BannerMensageCard text_p={"Aprovechá el descuento con el código: 50MILOFF"} />
+                <BannerMensageCard text_p={ "Pague em até 12 x sem juros"} />
             )}
         </>
     );
@@ -579,7 +579,7 @@ function observarCambiosCheckAndRenderII() {
 
     const observer = new MutationSummary(observerConfig);
 
-    checkAndRender(); // Llamar a checkAndRender al cargar la página por primera vez
+    checkAndRender();
 }
 checkAndRender();
 observarCambiosCheckAndRenderII();
